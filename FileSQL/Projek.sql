374a106058c3c30e4695daacb5d0d601d3a820f7
Firman <firmanadisusantoo@gmail.com>
2024-02-15 18:28:38 +0700

    feat:data

--- FileSQL/Projek.sql
+++ FileSQL/Projek.sql
@@ -4,15 +4,22 @@ use project;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
     phone_number VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     balance DOUBLE
 );
 
+
 CREATE TABLE transactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
     user_id INT,
-    phone_number VARCHAR(2055),
+    phone_number VARCHAR(255),
     amount DOUBLE,
     type VARCHAR(255)
 );
